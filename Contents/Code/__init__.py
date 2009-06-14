e2cc146841439beb81ec791f68e0f78a1830a482
Jesse Jaggars <jhjaggars2@jesse-jaggarss-macbook-pro.local>
2009-06-14 14:46:35 -0500

    wouldn't you know, the website changed

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,25 +6,29 @@ BASE_URL = "http://www.qwantz.com"
 CACHE_TIME = 3600
 archived = "new"
 
-####################################################################################################
 def Start():
-  Plugin.AddRequestHandler(PLUGIN_PREFIX, HandlePhotosRequest, "dinosaurcomics", "icon-default.png", "art-default.jpg")
-  Plugin.AddViewGroup("InfoList", viewMode="InfoList", contentType="items")
+    Plugin.AddRequestHandler(PLUGIN_PREFIX, HandlePhotosRequest, "dinosaurcomics", "icon-default.png", "art-default.jpg")
+    Plugin.AddViewGroup("InfoList", viewMode="InfoList", contentType="items")
 
-####################################################################################################
 def HandlePhotosRequest(pathNouns, count):
-  dir_ = MediaContainer("art-default.jpg", "InfoList", "dinosaurcomics")
-  global archived
-  if archived == "new":
-      archived = XML.ElementFromString(HTTP.GetCached(BASE_URL + "/archive/list.html", CACHE_TIME), True).xpath('//li//a[1]')[::-1]
+    dir_ = MediaContainer("art-default.jpg", "InfoList", "dinosaurcomics")
+    global archived
+    if archived == "new":
+        archived = XML.ElementFromString(HTTP.GetCached(BASE_URL + "/archive.php", CACHE_TIME), True).xpath('//a[contains(@href, "www.qwantz.com/index.php?comic=")]')
 
-  for item in archived[count*10:count*10+10]:
-    title = item.text
-    imgHTML = XML.ElementFromString(HTTP.GetCached(BASE_URL + item.get("href"), CACHE_TIME), True).xpath('//img[contains(@src, "comics")]')[0]
-    img = imgHTML.get("src")
-    desc = imgHTML.get("title")
-    ph = PhotoItem(img, title, desc, img)
-    dir_.AppendItem(ph)
-  dir_.AppendItem(DirectoryItem("next10","Next 10...",""))
+    for item in archived[count*10:count*10+10]:
+        try:
+            title = item.text
+            imgHTML = XML.ElementFromString(HTTP.GetCached(item.get("href"), CACHE_TIME), True).xpath('//img[contains(@class, "comic")]')[0]
+            img = imgHTML.get("src")
+            desc = imgHTML.get("title")
+            ph = PhotoItem(img, title, desc, img)
+            dir_.AppendItem(ph)
+        except Exception, e:
+            Log.Add("Exception caught when trying to fetch comic:")
+            Log.Add("\t" + str(e))
+            Log.Add("\t" + item.get("href"))
 
-  return dir_.ToXML()
+    dir_.AppendItem(DirectoryItem("next10","Next 10...",""))
+
+    return dir_.ToXML()
